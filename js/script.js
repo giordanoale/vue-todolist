9e75c509517ee6395e179819d193f78bee9aa818
giordanoale <a.giordano89@gmail.com>
2021-03-26 16:30:34 +0100

    Il programma stampa dall'array di cose da fare e permette di cancellare voci dalla lista premendo sulla X

--- js/script.js
+++ js/script.js
@@ -1,11 +1,19 @@
+// - stampare in pagina un item per ogni elemento contenuto in un array
+// - ogni item ha una "x" associata: cliccando su di essa, l'item viene rimosso dalla lista
+// - predisporre un input per aggiungere un nuovo item alla lista: digitando il tasto invio oppure ciccando su un pulsante, il testo digitato viene aggiunto alla lista
+
 var app = new Vue(
     {
         el:"#root",
         data: {
-            lista: ["Pane", "Latte","Corn Flakes"]
+            itemIndex: null,
+            lista: ["Pane", "Latte","Corn Flakes"],
+            addItem: "",
         },
-        method: {
-
+        methods: {
+            deleteItem: function(itemIndex) {
+                this.lista.splice(itemIndex, 1);
+            },
         }
     }
 );
